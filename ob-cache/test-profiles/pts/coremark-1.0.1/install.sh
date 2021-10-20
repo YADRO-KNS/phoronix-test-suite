182271d4636857cbd28d5fd07b0c4ea4bfc8b687
Romanov Alexey <romanov.alexey2000@gmail.com>
2021-10-20 16:59:40 +0300

    ob-cache: coremark-1.0.1 test profile runs in one thread and in several

--- ob-cache/test-profiles/pts/coremark-1.0.1/install.sh
+++ ob-cache/test-profiles/pts/coremark-1.0.1/install.sh
@@ -4,11 +4,13 @@ unzip -o coremark-20190727.zip
 cd coremark-master
 if [ $OS_TYPE = "BSD" ]
 then
-        gmake XCFLAGS="$CFLAGS -DMULTITHREAD=$NUM_CPU_CORES -DUSE_FORK=1" compile PORT_DIR=linux64
+    gmake XCFLAGS="$CFLAGS -DMULTITHREAD=1 -DUSE_FORK=1" compile PORT_DIR=linux64
+    gmake XCFLAGS="$CFLAGS -DMULTITHREAD=$NUM_CPU_CORES -DUSE_FORK=1" compile PORT_DIR=linux64
 else
-        make XCFLAGS="$CFLAGS -DMULTITHREAD=$NUM_CPU_CORES -DUSE_FORK=1" compile
-
+    make XCFLAGS="$CFLAGS -DMULTITHREAD=1 -DUSE_FORK=1" compile
+    make XCFLAGS="$CFLAGS -DMULTITHREAD=$NUM_CPU_CORES -DUSE_FORK=1" OUTNAME="coremark-threaded.exe" compile
 fi
+
 echo $? > ~/install-exit-status
 cd ~
 
@@ -17,3 +19,14 @@ cd coremark-master
 ./coremark.exe > \$LOG_FILE 2>&1
 echo \$? > ~/test-exit-status" > coremark
 chmod +x coremark
+
+THREADED_DIR="../coremark-1.0.1-threaded"
+mkdir -p ${THREADED_DIR}
+cp -r . ${THREADED_DIR}
+cd ${THREADED_DIR}
+
+echo "#!/bin/sh
+cd coremark-master
+./coremark-threaded.exe > \$LOG_FILE 2>&1
+echo \$? > ~/test-exit-status" > coremark-1.0.1-threaded
+chmod +x coremark-1.0.1-threaded
