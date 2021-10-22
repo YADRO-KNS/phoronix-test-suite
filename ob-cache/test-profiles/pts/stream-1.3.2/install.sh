ff2e0fe71c0d58a5808144a178ad1672e87b4ea5
Romanov Alexey <romanov.alexey2000@gmail.com>
2021-10-22 14:41:15 +0300

    ob-cache: stream-1.3.2 test profile runs in one thread and serveral

--- ob-cache/test-profiles/pts/stream-1.3.2/install.sh
+++ ob-cache/test-profiles/pts/stream-1.3.2/install.sh
@@ -4,12 +4,12 @@ tar -jxf stream-2013-01-17.tar.bz2
 
 if [ "X$CFLAGS_OVERRIDE" = "X" ]
 then
-          CFLAGS="$CFLAGS -O3 -march=native"
+    CFLAGS="$CFLAGS -march=native"
 else
-          CFLAGS="$CFLAGS_OVERRIDE"
+    CFLAGS="$CFLAGS_OVERRIDE"
 fi
 
-STREAM_ARRAY_SIZE=100000000
+STREAM_ARRAY_SIZE=10000000
 L3_CACHE_SIZE=`getconf LEVEL3_CACHE_SIZE`
 SIZE_BASED_ON_L3=$((L3_CACHE_SIZE * 4))
 
@@ -18,11 +18,24 @@ then
      STREAM_ARRAY_SIZE=$SIZE_BASED_ON_L3
 fi
 
-cc stream.c -DSTREAM_ARRAY_SIZE=$STREAM_ARRAY_SIZE -DNTIMES=100 $CFLAGS -fopenmp -o stream-bin
-echo \$? > ~/install-exit-status
+cc stream.c -DSTREAM_ARRAY_SIZE=$STREAM_ARRAY_SIZE -DNTIMES=100 $CFLAGS -o stream-bin
+echo $? > ~/install-exit-status
+
+cc stream.c -DSTREAM_ARRAY_SIZE=$STREAM_ARRAY_SIZE -DNTIMES=100 $CFLAGS -fopenmp -o stream-bin-threaded
+echo $? > ~/install-exit-status
 
 echo "#!/bin/sh
-export OMP_NUM_THREADS=\$NUM_CPU_CORES
 ./stream-bin > \$LOG_FILE 2>&1
 echo \$? > ~/test-exit-status" > stream
 chmod +x stream
+
+THREADED_DIR="../stream-1.3.2-threaded"
+mkdir -p ${THREADED_DIR}
+cp -r . ${THREADED_DIR}
+cd ${THREADED_DIR}
+
+echo "#!/bin/sh
+export OMP_NUM_THREADS=\$NUM_CPU_CORES
+./stream-bin-threaded > \$LOG_FILE 2>&1
+echo \$? > ~/test-exit-status" > stream-1.3.2-threaded
+chmod +x stream-1.3.2-threaded
